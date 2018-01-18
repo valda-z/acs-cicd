ad2d930baf53e6a01b6736378bf45048e49212bd
valda-z <valdemar.zavadsky@microsoft.com>
2018-01-18 14:56:13 +0100

    run.sh fix jenkins config

--- run.sh
+++ run.sh
@@ -319,13 +319,13 @@ done
 echo -n "${JENKINS_KEY}"
 echo ""
 
-kubectl exec ${KUBE_JENKINS} -- cat /var/jenkins_config/config.xml > /tmp/config.xml
-sed -i.bak s/kubernetes.default/kubernetese.default.svc/g /tmp/config.xml
-kubectl cp /tmp/config.xml ${KUBE_JENKINS}:/var/jenkins_config/config.xml
+retry_until_successful kubectl exec ${KUBE_JENKINS} -- cat /var/jenkins_config/config.xml > /tmp/config.xml
+sed -i.bak s/kubernetes.default/kubernetes.default.svc/g /tmp/config.xml
+retry_until_successful kubectl cp /tmp/config.xml ${KUBE_JENKINS}:/var/jenkins_config/config.xml
 
-kubectl exec ${KUBE_JENKINS} -- cat /var/jenkins_home/config.xml > /tmp/config.xml
-sed -i.bak s/kubernetes.default/kubernetese.default.svc/g /tmp/config.xml
-kubectl cp /tmp/config.xml ${KUBE_JENKINS}:/var/jenkins_home/config.xml
+retry_until_successful kubectl exec ${KUBE_JENKINS} -- cat /var/jenkins_home/config.xml > /tmp/config.xml
+sed -i.bak s/kubernetes.default/kubernetes.default.svc/g /tmp/config.xml
+retry_until_successful kubectl cp /tmp/config.xml ${KUBE_JENKINS}:/var/jenkins_home/config.xml
 
 ### install jenkins plugins
 run_cli_command "install-plugin pipeline-utility-steps -deploy"
